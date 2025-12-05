f8c49627a485c1eb684e8adbb46363cc163820c4
OscarFouz <240739430+OscarFouz@users.noreply.github.com>
2025-12-05 12:39:24 +0100

    feat: Primer commit ApartmentPredictor con clases basicas

---
+++ src/main/java/com/example/apartment_predictor/Review.java
@@ -0,0 +1,53 @@
+package com.example.apartment_predictor;
+
+public class Review {
+    private int idReview;
+    private int idApartmentReview;
+    private int idReviewerReview;
+    private String texto;
+    private int valor;
+
+    //Constructors
+    public Review() {
+    }
+
+    public Review(int idReview, int idApartmentReview, int idReviewerReview, String texto, int valor) {
+        this.idReview = idReview;
+        this.idApartmentReview = idApartmentReview;
+        this.idReviewerReview = idReviewerReview;
+        this.texto = texto;
+        this.valor = valor;
+    }
+
+    //Setters
+    public void setIdApartmentReview(int idApartmentReview) {this.idApartmentReview = idApartmentReview; }
+
+    public void setIdReviewerReview(int idReviewerReview) {this.idReviewerReview = idReviewerReview; }
+
+    public void setTexto(String texto) {this.texto = texto; }
+
+    public void setValor(int valor) {this.valor = valor; }
+
+    //Getters
+
+    public int getIdReview() {return idReview; }
+
+    public int getIdApartmentReview() {return idApartmentReview; }
+
+    public int getIdReviewerReview() {return idReviewerReview; }
+
+    public String getTexto() {return texto; }
+
+    public int getValor() {return valor; }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "idReview=" + idReview +
+                ", idApartmentReview=" + idApartmentReview +
+                ", idReviewerReview=" + idReviewerReview +
+                ", texto='" + texto + '\'' +
+                ", valor=" + valor +
+                '}';
+    }
+}
